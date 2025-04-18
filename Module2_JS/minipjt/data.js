84223d957244f7513d2b8f9a63a0f0af66ad4056
Hoang Hai Duong <hhduong183@gmail.com>
2025-04-18 21:06:14 +0700

    new file:   Module2_JS/minipjt/data.js 	modified:   Module2_JS/minipjt/index.html 	modified:   Module2_JS/minipjt/index.js

---
+++ Module2_JS/minipjt/data.js
@@ -0,0 +1,7 @@
+// Danh sách sinh viên
+const studentsList = [
+    { name: "Nguyen Van A", gender: "Nam", mathScore: 8, englishScore: 9, literatureScore: 7 },
+    { name: "Nguyen Van AB", gender: "Nam", mathScore: 7, englishScore: 8, literatureScore: 6 },
+    { name: "Nguyen Thi CD", gender: "Nữ", mathScore: 9, englishScore: 9, literatureScore: 8 }
+];
+export default studentsList;
